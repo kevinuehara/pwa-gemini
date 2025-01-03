e2cfba5aaeab1f884b5a2d0cec00df01e7749f49
kevinuehara <uehara.kevin@gmail.com>
2025-01-03 17:05:09 -0300

    fix: Adding pwa resources

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  manifest: {
+    name: "React-vite-app",
+    short_name: "react-vite-app",
+    description: "Gemini Chatbot",
+    icons: [
+      {
+        src: "/gemini-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/gemini-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/gemini-180.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/gemini-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 });
