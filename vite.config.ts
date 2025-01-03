ec020e29ecb76bd1ffb0f9d5b402090564b16c69
kevinuehara <uehara.kevin@gmail.com>
2025-01-03 16:49:37 -0300

    fix: remove pwa resource

--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    preserveSymlinks: true,
-  },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "React-vite-app",
-        short_name: "react-vite-app",
-        description: "Gemini Chatbot",
-        icons: [
-          {
-            src: "/gemini-192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "favicon",
-          },
-          {
-            src: "/gemini-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "favicon",
-          },
-          {
-            src: "/gemini-180.png",
-            sizes: "180x180",
-            type: "image/png",
-            purpose: "apple touch icon",
-          },
-          {
-            src: "/gemini-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-        theme_color: "#171717",
-        background_color: "#f0e7db",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        orientation: "portrait",
-      },
-    }),
-  ],
+  plugins: [react()],
 });
